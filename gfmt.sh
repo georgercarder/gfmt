aa7262eb7017589eac5dca8339d7003ba7903226
George Carder <georgercarder@gmail.com>
2020-07-14 16:18:12 -0700

    wait

--- gfmt.sh
+++ gfmt.sh
@@ -7,9 +7,11 @@ dirs=$(go list -f {{.Dir}} ./...)
 for d in $dirs; 
 	do goimports -w $d/*.go &
 done
+wait
 
 #detect and print files w offending style
 #echo $dirs
 for d in $dirs;
 	do gstyle $d/*.go &
 done
+wait

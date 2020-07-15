d7b9ae0c9c28cccb0d8388a382b03ac3f704b835
George Carder <georgercarder@gmail.com>
2020-07-15 14:06:00 -0700

    err catch in install

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 go get golang.org/x/tools/cmd/goimports
-
+errFlag=$?
 ln scripts/gfmt.sh /usr/bin/gfmt
+errFlag=$(($errFlag||$?))
 ln scripts/gstyle.sh /usr/bin/gstyle
-
+errFlag=$(($errFlag||$?))
+if [ ! $errFlag -eq 0 ]; then
+	echo ""
+	echo "installation unsuccessful."
+	echo "try:"
+	echo "  - using sudo"
+	echo "  - removing symlinks /usr/bin/gfmt /usr/bin/gstyle and try again"
+	echo ""
+	exit 1
+fi
 echo "installation complete."
 echo ""
 echo "use: run 'gfmt' from any golang folder."

e6f47eac3e49ddfa56e55918e3391468be87e217
George Carder <georgercarder@gmail.com>
2020-08-13 22:29:44 -0700

    update install

--- install.sh
+++ install.sh
@@ -1,19 +1,43 @@
 #!/bin/sh
 
+err_not_installed() {
+	app=$1
+	echo ""
+	echo "installation unsuccessful"
+	echo "  $app is not available"
+	echo "  check your $app installation"
+}
+
+link() {
+	script=$1
+	name=$(echo $script | sed 's/\..*//g')
+	if [ "$name" != "gdp" ]; then 
+		name="gdp_"$name
+	fi
+	binPath=/usr/bin/$name
+	if [ ! -z "$binPath" ]; then
+		rm $binPath 2> /dev/null
+	fi
+	ln scripts/$script $binPath
+}
+
 go get golang.org/x/tools/cmd/goimports
 errFlag=$?
-ln scripts/gfmt.sh /usr/bin/gfmt
-errFlag=$(($errFlag||$?))
-ln scripts/gstyle.sh /usr/bin/gstyle
-errFlag=$(($errFlag||$?))
 if [ ! $errFlag -eq 0 ]; then
+	err_not_installed "goimports"
+	exit 1
+fi
+
+link gfmt.sh
+ext=$?
+link gstyle.sh
+ext=$(($ext||$?))
+if [ ! $ext -eq 0 ]; then
 	echo ""
-	echo "installation unsuccessful."
-	echo "try:"
+	echo "  installation unsuccessful"
+	echo "  try:"
 	echo "  - using sudo"
 	echo "  - removing symlinks /usr/bin/gfmt /usr/bin/gstyle and try again"
-	echo "  - checking go version go1.14.4 linux/amd64 is installed"
-	echo "  - checking system has bash"
 	echo ""
 	exit 1
 fi
